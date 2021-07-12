842a2d9224891d5c58a11ed2ac5034220c2e5d4d
Christopher <christopher.black.2209@googlemail.com>
2021-07-12 23:32:21 +0100

    commit tree 475c7aab5c513c7778fd520282b64ecca0856f24

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,22 @@
 class Pokemon
+    attr_accessor :name, :type, :db, :id
+
+    def initialize(id: nil, name:, type:, db:)
+        @name = name
+        @type = type
+        @id = id
+    end 
+
+    def self.save(name, type, db)
+        sql = "INSERT INTO pokemon (name, type) VALUES (?,?)"
+        db.execute(sql, name, type)
+        @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0] 
+    end 
+
+    def self.find(id, db)
+        sql = "SELECT * FROM pokemon WHERE id = ?"
+        db_record = db.execute(sql, id).flatten 
+        new_pokemon = Pokemon.new(id: db_record[0], name: db_record[1], type: db_record[2], db: db)
+    end 
+
 end
